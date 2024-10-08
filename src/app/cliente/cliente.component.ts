0f8d47f1cfcf8d9fd5888d1def2874d7a2b4480c
NicolasHincapie <nicolas.hincapiej@javeriana.edu.co>
2024-10-07 19:02:47 -0500

    Correcciones CSS Cliente

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -15,6 +15,8 @@ export class ClienteComponent {
   @Input()
   cliente!: Cliente;
 
+  selectedMascota: any = null;
+
   constructor(
     private clienteService: ClienteService, 
     private router: Router, 
@@ -28,12 +30,39 @@ export class ClienteComponent {
       this.clienteService.findById(id).pipe(
         mergeMap((clienteInfo) => {
           this.cliente = clienteInfo;
-          console.log(this.cliente);
+          console.log('Cliente Cargado', this.cliente);
           return this.mascotaService.findClienteMascota(id);
         })
       ).subscribe((mascotas) => {
         this.cliente.mascotas = mascotas;
-      })
-    })
+        console.log('Mascotas Cargadas', this.cliente.mascotas);
+      });
+    });
+  }
+
+  mostrarMascota(event: any) {
+    const selectedId = Number(event.target.value); // Convierte a número
+
+    if (selectedId && this.cliente && this.cliente.mascotas) {
+      // Busca la mascota en la lista local
+      this.selectedMascota = this.cliente.mascotas.find((mascota) => mascota.id === selectedId);
+
+      // Si no se encuentra en la lista local, busca en el servicio
+      if (!this.selectedMascota) {
+        this.mascotaService.findById(selectedId).subscribe(
+          (mascota) => {
+            this.selectedMascota = mascota; // Asigna la mascota recibida del observable
+            console.log('Mascota seleccionada desde el servicio:', this.selectedMascota);
+          },
+          (error) => {
+            console.error('Error al cargar la mascota:', error); // Manejo de errores
+          }
+        );
+      } else {
+        console.log('Mascota seleccionada desde la lista local:', this.selectedMascota);
+      }
+    } else {
+      this.selectedMascota = null; // Restablecer si no hay selección
+    }
   }
 }
