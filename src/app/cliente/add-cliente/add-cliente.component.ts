8a95c0908cc687dbf35d3e0b85582ca227ed4002
NicolasHincapie <nicolas.hincapiej@javeriana.edu.co>
2024-10-27 14:04:20 -0500

    Pruebas Front

--- src/app/cliente/add-cliente/add-cliente.component.ts
+++ src/app/cliente/add-cliente/add-cliente.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { Cliente } from 'src/app/model/cliente';
 import { ClienteService } from 'src/app/service/cliente.service';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-add-cliente',
@@ -9,22 +10,42 @@ import { ClienteService } from 'src/app/service/cliente.service';
   styleUrls: ['./add-cliente.component.css']
 })
 export class AddClienteComponent {
-
-  constructor(private clienteService: ClienteService, private router: Router) {}
+  clienteForm: FormGroup;
 
   @Output() addClienteEvent = new EventEmitter<Cliente>();
 
-  formCliente: Cliente = {
-    id: 0,
-    nombre: '',
-    email: '',
-    cedula: 0,
-    celular: 0
-  };
+  constructor(
+    private clienteService: ClienteService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.clienteForm = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email, this.emailDomainValidator]],
+      cedula: [0, Validators.required],
+      celular: [0, Validators.required]
+    });
+  }
+
+  // Validador personalizado para el dominio del correo
+  emailDomainValidator(control: AbstractControl): ValidationErrors | null {
+    const email: string = control.value;
+    const domainPattern = /^[^\s@]+@[^\s@]+\.(com|org|edu|net)$/;  // Acepta solo dominios específicos
+
+    if (!domainPattern.test(email)) {
+      return { invalidDomain: true }; // Retorna un error si el dominio es inválido
+    }
+    return null; // Si no hay error, retorna null
+  }
 
   addCliente() {
-    this.clienteService.addCliente(this.formCliente).subscribe({
-      complete: () => this.router.navigate(['/clientes'])
-    });
+    if (this.clienteForm.valid) {
+      const nuevoCliente: Cliente = this.clienteForm.value;
+      this.clienteService.addCliente(nuevoCliente).subscribe({
+        complete: () => this.router.navigate(['/clientes'])
+      });
+    } else {
+      console.log('Formulario inválido');
+    }
   }
 }
